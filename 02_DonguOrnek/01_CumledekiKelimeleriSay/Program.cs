a9cc24c5c670e0470cf2efb35a5cfd065122324b
Hüseyin Emecan <emecanh@gmail.com>
2018-11-24 19:10:55 +0300

    Döngüler Örnek Çalışmalar
    Örnek çalışma kodları eklendi

---
+++ 02_DonguOrnek/01_CumledekiKelimeleriSay/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_CumledekiKelimeleriSay
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // girilen cumledeki kelimeleri sayan program
+            // MERHABA DÜNYA
+            // STRİNG içindeki char a erişme 
+            //string[indexno]
+
+
+            string cumle;
+            Console.Write("Cümlenizi Girin : ");
+            cumle = Console.ReadLine().Trim().ToLower();
+
+            // butun harflere tek tek ulaş ? 
+            int indexno = 0;
+            int kelimesayaci = 0;
+            int harfsayisi = 0;
+        kontrolet:
+            if (cumle[indexno] == ' ' && cumle[indexno + 1] != ' ')
+            {
+                kelimesayaci++;
+            }
+            else harfsayisi++;
+            indexno++;
+            if (indexno < cumle.Length) goto kontrolet;
+
+            Console.WriteLine("Girilen Cümle {0} kelimelidir {1} harflidir", kelimesayaci + 1, harfsayisi);
+
+            Console.ReadKey();
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ 02_DonguOrnek/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_DonguOrnek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            // girilen sayıdan geriye dogru 3 er 3 er yazdır
+
+            Console.WriteLine("Sayı Girin : ");
+            int sayi = int.Parse(Console.ReadLine());
+
+            for (int i = sayi; i >= 0; i -= 3)
+            {
+                Console.WriteLine(i);
+            }
+            // Rastgele üretilen sayıya kadar olan sayıları ekrana yaz 
+
+            Random rnd = new Random();
+
+            int sinir = rnd.Next(20);
+            for (int i = 0; i < sinir; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            // rastgele 5 adet sayı üret ? 
+
+
+            int uretilen = rnd.Next(50);
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(uretilen);
+            }
+
+
+            // NOT: sayac yukarda artmak zorunda değil !
+
+            for (int i = 0; i < 10; )
+            {
+                i++;
+            }
+
+
+            // rastgele 5 adet CİFT sayı üret
+
+         
+            for (int i = 0; i < 5; )
+            {
+                uretilen = rnd.Next(50);
+                if (uretilen % 2 == 0)
+                {
+                    Console.WriteLine(uretilen);
+                    i++;
+                }
+            }
+
+            // NOT : sorgu birden fazla olabilir
+
+
+            int girilensayi = 5;
+
+            for (int i = 0; i < 5 && girilensayi!=10; i++)
+            {
+                Console.WriteLine("sayı " );
+                girilensayi = int.Parse(Console.ReadLine());
+            }
+
+            for (int i = 0, y=0; i < 5 && y>-15; i++,y-=3)
+            {
+
+
+            }
+
+
+            // NOT: donguler iç içe kullanılabılır
+
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j   = 0; j < 20; j++)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            while (true)
+            {
+                for (int i = 0; i < length; i++)
+                {
+
+                   
+                }
+            }
+
+            for (int i = 0; i < length; i++)
+            {
+                while (true)
+                {
+
+                }
+            }
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
